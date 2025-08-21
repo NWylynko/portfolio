4aa11c7cbe3377f26f6f9678b8b61429dc94fd8f
Nick Wylynko <nick@wylynko.com>
2025-08-21 09:07:09 +0000

    show active tab state

--- app/(projects)/Nav.tsx
+++ app/(projects)/Nav.tsx
@@ -11,10 +11,10 @@ export const Nav = () => {
         <nav className="px-2">
             <ul className="flex flex-row gap-2 overflow-y-auto">
                 {projects.map((project, index) => (
-                    <li key={index} data-active={project.urls.includes(pathname)} className="border-t border-x rounded-t-2xl mt-1 hover:mt-0 transition-all cursor-pointer">
+                    <li key={index} data-active={project.urls.includes(pathname)} className="border-t border-x border-dashed rounded-t-2xl mt-1 hover:mt-0 transition-all cursor-pointer data-[active=true]:border-solid data-[active=true]:shadow">
                         <Link href={project.urls[0]} className="px-2 py-1 flex flex-row items-end gap-2">
                             <span className="font-medium text-lg -mb-1 whitespace-nowrap">{project.name}</span>
-                            <span className="text-sm text-gray-400">{project.year}</span>
+                            <span className="text-xs text-gray-400">{project.year}</span>
                         </Link>
                     </li>
                 ))}
