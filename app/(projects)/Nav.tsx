40dff349ac29a8ad2196b2d82bc719b497dbe691
Nick Wylynko <nick@wylynko.com>
2025-08-21 09:40:56 +0000

    refinement

--- app/(projects)/Nav.tsx
+++ app/(projects)/Nav.tsx
@@ -9,9 +9,11 @@ export const Nav = () => {
 
     return (
         <nav className="px-2">
-            <ul className="flex flex-row gap-2 overflow-y-auto">
+            <ul className="flex flex-row gap-2 overflow-y-auto overflow-x-visible">
                 {projects.map((project, index) => (
-                    <li key={index} data-active={project.urls.includes(pathname)} className="border-t border-x border-dashed rounded-t-2xl mt-1 hover:mt-0 transition-all cursor-pointer data-[active=true]:border-solid data-[active=true]:shadow">
+                    <li key={index} 
+                        data-active={project.urls.includes(pathname)} 
+                        className="border-t border-x border-gray-600 border-dashed rounded-t-2xl mt-1 hover:mt-0 transition-all cursor-pointer data-[active=true]:border-solid data-[active=true]:mt-0 bg-white">
                         <Link href={project.urls[0]} className="px-2 py-1 flex flex-row items-end gap-2">
                             <span className="font-medium text-lg -mb-1 whitespace-nowrap">{project.name}</span>
                             <span className="text-xs text-gray-400">{project.year}</span>
